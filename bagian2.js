f9a95158a986d2cb23bcd78fb1f41b01442caab6
Sava Adiwidya Maulana <savsavva58@gmail.com>
2026-09-10 16:12:02 +0700

    latihan_2.2

--- bagian2.js
+++ bagian2.js
@@ -4,5 +4,12 @@ const products = [
     {id:3, title: "Headphone", price: 100, category: "audio", stock: 3},
 ]
 
+function findProductById (product, id){
+    return products.find(product => product.id === id)
+}
+
+let hasilPencarian = findProductById (products, 3)
+console.log(hasilPencarian);
+
 const lowStockProducts = products.filter(product => product.stock < 10)
 console.log("produk dengan stock sedikit: ",lowStockProducts)
