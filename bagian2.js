0d0a6552beb056325c31adb9e848e0e7cbbde8bb
Sava Adiwidya Maulana <savsavva58@gmail.com>
2026-09-10 15:56:57 +0700

    latihan_2.1

---
+++ bagian2.js
@@ -0,0 +1,8 @@
+const products = [
+    {id:1, title: "laptop", price: 1200, category: "laptops", stock: 5},
+    {id:2, title: "Smartphone", price: 800, category: "phones", stock: 15},
+    {id:3, title: "Headphone", price: 100, category: "audio", stock: 3},
+]
+
+const lowStockProducts = products.filter(product => product.stock < 10)
+console.log("produk dengan stock sedikit: ",lowStockProducts)
