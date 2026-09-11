4ef00046d1745dbd7cf520b172bb78fb23fbc55f
Sava Adiwidya Maulana <savsavva58@gmail.com>
2026-09-11 20:25:22 +0700

    latihan_7.2

--- bagian7.js
+++ bagian7.js
@@ -19,4 +19,32 @@ function binarySearch(arr, target) {
 console.log("Posisi angka 55:", binarySearch(dataTerurut, 55));
 
 
+const daftarProduk = [
+  { id: 1, title: "baju", price: 150000 },
+  { id: 2, title: "celana", price: 450000 },
+  { id: 2, title: "topi", price: 300000 },
+];
+const sortedProducts = [...daftarProduk].sort((a, b) => a.price - b.price);
+
+function binarySearchByPrice(sortedArray, targetPrice) {
+  let left = 0;
+  let right = sortedArray.length - 1;
+
+  while (left <= right) {
+    const mid = left + Math.floor((right - left) / 2);
+    const currentPrice = sortedArray[mid].price;
+
+    if (currentPrice === targetPrice) {
+      return sortedArray[mid];
+    }
+    if (currentPrice < targetPrice) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return null;
+}
+const detailProduk = binarySearchByPrice(sortedProducts, 150000);
+console.log(detailProduk);
 
