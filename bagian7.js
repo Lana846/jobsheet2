dc2503760673a6a47433ab59b3b8194377326e90
Sava Adiwidya Maulana <savsavva58@gmail.com>
2026-09-11 20:24:39 +0700

    latihan_7.1

---
+++ bagian7.js
@@ -0,0 +1,22 @@
+const dataTerurut = [10, 24, 35, 42, 55, 68, 71, 89];
+
+function binarySearch(arr, target) {
+  let left = 0;
+  let right = arr.length - 1;
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    if (arr[mid] === target) {
+      return mid;
+    }
+    if (arr[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return -1;
+}
+console.log("Posisi angka 55:", binarySearch(dataTerurut, 55));
+
+
+
