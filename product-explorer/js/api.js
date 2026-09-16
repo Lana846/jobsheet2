56789a521c55e4869d68bb8477a0c0356c773a06
Sava Adiwidya Maulana <savsavva58@gmail.com>
2026-09-16 22:40:46 +0700

    latihan_24

--- product-explorer/js/api.js
+++ product-explorer/js/api.js
@@ -1,16 +1,15 @@
-import { products } from "./data.js";
-
-
 export async function fetchProducts() {
+    try {
+        const response = await fetch("https://dummyjson.com/products?limit=30");
 
-    return new Promise((resolve) => {
-
-        setTimeout(() => {
-
-            resolve(products);
-
-        }, 500);
-
-    });
-
+        if (!response.ok) {
+            throw new Error(`HTTP error: ${response.status}`);
+        }
+        
+        const data = await response.json();
+        return data.products; 
+    } catch (error) {
+        console.error("Gagal mengambil data:", error);
+        throw error; 
+    }
 }
